3c3db8f6d6381186d6af8cd1a7fcfae80ebba549
CristianB13 <cristian36200119@gmail.com>
2022-12-28 21:56:59 +0200

    add flip animation to countdown-timer-card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { CountdownTimerComponent } from './components/countdown-timer/countdown-timer.component';
@@ -12,7 +13,8 @@ import { CountdownTimerCardComponent } from './components/countdown-timer-card/c
     CountdownTimerCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/countdown-timer-card/countdown-timer-card.component.ts
+++ src/app/components/countdown-timer-card/countdown-timer-card.component.ts
@@ -1,11 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { animate, query, style, transition, trigger } from '@angular/animations';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-countdown-timer-card',
   templateUrl: './countdown-timer-card.component.html',
-  styleUrls: ['./countdown-timer-card.component.scss']
+  styleUrls: ['./countdown-timer-card.component.scss'],
+  animations: [
+    trigger('flip', [
+      transition("* => 1", animate('300ms linear', style({transform: 'rotateX(-180deg)'})))
+    ])
+  ]
 })
-export class CountdownTimerCardComponent {
+export class CountdownTimerCardComponent implements OnInit{
   @Input() value!: number;
   @Input() label!: string;
+  previousValue!: number;
+
+  ngOnInit(): void {
+    this.previousValue = this.value;
+  }
 }
