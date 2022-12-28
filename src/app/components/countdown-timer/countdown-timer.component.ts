2268909c87e2b5f6bc1130b99ea030b3760ca862
CristianB13 <cristian36200119@gmail.com>
2022-12-29 01:02:02 +0200

    add countdown, animations working well only on Chrome

--- src/app/components/countdown-timer/countdown-timer.component.ts
+++ src/app/components/countdown-timer/countdown-timer.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-countdown-timer',
   templateUrl: './countdown-timer.component.html',
   styleUrls: ['./countdown-timer.component.scss']
 })
-export class CountdownTimerComponent {
+export class CountdownTimerComponent implements OnInit, OnDestroy {
+  @Input() launchDate: Date = new Date(new Date().getFullYear()+1, 0, 1)
+  private subscription = new Subscription;
+  days!: number;
+  hours!: number;
+  minutes!: number;
+  seconds!: number;
+  
+  ngOnInit(): void {
+    this.updateValues();
+    console.log(this.launchDate)
+    this.subscription = interval(1000).subscribe(x => this.updateValues());
+  }
 
+  updateValues() {
+    let currentDate = new Date();
+    let delta = (this.launchDate.getTime() - currentDate.getTime()) / 1000;
+    delta = Math.max(0, delta)
+
+    this.days = Math.floor(delta / 86400);
+    delta -= this.days * 86400;
+
+    this.hours = Math.floor(delta / 3600) % 24;
+    delta -= this.hours * 3600;
+
+    this.minutes = Math.floor(delta / 60) % 60;
+    delta -= this.minutes * 60;
+
+    this.seconds = Math.floor(delta % 60);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
